93fdcfe83ac7a8f0067d23fa683c29796406eb12
Brian Mgrdichian <brianmgrdichian@gmail.com>
2019-01-04 03:07:30 -0800

    Added white to LED, split LEDs into 2 different shades

--- CapacitiveTouchLEDs.ino
+++ CapacitiveTouchLEDs.ino
@@ -2,29 +2,40 @@
 #include <Adafruit_NeoPixel.h>
 
 CapacitiveSensor capSensor = CapacitiveSensor(4,2);
-Adafruit_NeoPixel pixels = Adafruit_NeoPixel(1, 13, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(4, 13, NEO_GRBW + NEO_KHZ800);
 
 int state = 0;
 
 void setup() {
   capSensor.set_CS_AutocaL_Millis(0xFFFFFFFF);
   pixels.begin();
+  pixels.show();
+  Serial.begin(9600);
 }
 
 void loop() {
   long capReading =  capSensor.capacitiveSensor(30);
-
-  if (capReading > 120){
+  
+  if (capReading > 125){
     if (state == 0){
-      pixels.setPixelColor(0, pixels.Color(255,40,0));
-      state = 1;
+      Serial.println(capReading);
+      pixels.setPixelColor(0,255,30,0,255);
+      pixels.setPixelColor(1,255,30,0,0);
+      pixels.setPixelColor(2,255,30,0,0);
+      pixels.setPixelColor(3,255,30,0,255);
       pixels.show();
+      state = 1;
       delay(500);
     } else if (state == 1){
-      pixels.setPixelColor(0, pixels.Color(0,0,0));
+      Serial.println(capReading);
+      for(int i=0;i<4;i++){
+        pixels.setPixelColor(i, pixels.Color(0,0,0,0));
+        pixels.show();
+      }
       state = 0;
-      pixels.show();
       delay(500);
     }
+  } else {
+    delay(15);
   }
 }
