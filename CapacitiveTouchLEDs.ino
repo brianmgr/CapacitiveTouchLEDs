270f8916c64cff399416fcb600c8ff2a21026654
Brian Mgrdichian <brianmgrdichian@gmail.com>
2019-01-05 18:27:34 -0800

    Attiny85 pinout

--- CapacitiveTouchLEDs.ino
+++ CapacitiveTouchLEDs.ino
@@ -1,8 +1,11 @@
 #include <CapacitiveSensor.h>
 #include <Adafruit_NeoPixel.h>
 
-CapacitiveSensor capSensor = CapacitiveSensor(4,2);
-Adafruit_NeoPixel pixels = Adafruit_NeoPixel(4, 13, NEO_GRBW + NEO_KHZ800);
+//Pins 0 and 4 for sensor
+CapacitiveSensor capSensor = CapacitiveSensor(0,4);
+
+//Pin 2 for data to LEDs
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(4, 2, NEO_GRBW + NEO_KHZ800);
 
 int state = 0;
 
@@ -10,7 +13,6 @@ void setup() {
   capSensor.set_CS_AutocaL_Millis(0xFFFFFFFF);
   pixels.begin();
   pixels.show();
-  Serial.begin(9600);
 }
 
 void loop() {
@@ -18,16 +20,14 @@ void loop() {
   
   if (capReading > 125){
     if (state == 0){
-      Serial.println(capReading);
-      pixels.setPixelColor(0,255,30,0,255);
-      pixels.setPixelColor(1,255,30,0,0);
+      pixels.setPixelColor(0,255,0,0,0);
+      pixels.setPixelColor(1,0,0,0,255);
       pixels.setPixelColor(2,255,30,0,0);
       pixels.setPixelColor(3,255,30,0,255);
       pixels.show();
       state = 1;
       delay(500);
     } else if (state == 1){
-      Serial.println(capReading);
       for(int i=0;i<4;i++){
         pixels.setPixelColor(i, pixels.Color(0,0,0,0));
         pixels.show();
